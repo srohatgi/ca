4be99e47effe037b9af34d10828e598bff36cb62
srohatgi <sumeet_rohatgi@hotmail.com>
2012-03-30 11:06:33 -0700

    added mapping function

--- www/common.js
+++ www/common.js
@@ -11,32 +11,70 @@ var isWindows = false;
 
 // Store the device's uuid
 var deviceUUID;
+var currentUrl;
+var internetInterval;
 
-function init() {
-  // Add an event listener for deviceready
-  document.addEventListener("deviceready", onDeviceReady, false);
+function init(url) {
+  if (typeof url != 'string') {
+    currentUrl = location.href;
+  } else {
+    currentUrl = url;
+  }
+
+  if (isPhoneGapReady) {
+    onDeviceReady();
+  } else {
+    // Add an event listener for deviceready
+    document.addEventListener("deviceready", onDeviceReady, false);
+  }
 }
 
 function onDeviceReady() {
-  // set to true
   isPhoneGapReady = true;
   deviceUUID = device.uuid;
   deviceDetection();
   networkDetection();
-  
-  // attach events for online and offline detection
-  document.addEventListener("online", onOnline, false);
-  document.addEventListener("offline", onOffline, false);
+  executeEvents();
+  executeCallback();
+}
 
-  // set a timer to check the network status
-  internetInterval = window.setInterval(function() {
-    if (navigator.network.connection.type != Connection.NONE) {
-      onOnline();
-    } else {
-      onOffline();
+function executeEvents() {
+  if (isPhoneGapReady) {
+    // attach events for online and offline detection
+    document.addEventListener("online", onOnline, false);
+    document.addEventListener("offline", onOffline, false);
+
+    // attach events for pause and resume detection
+    document.addEventListener("pause", onPause, false);
+    document.addEventListener("resume", onResume, false);
+
+    // set a timer to check the network status
+    internetInterval = window.setInterval(function() {
+      if (navigator.network.connection.type != Connection.NONE) {
+        onOnline();
+      } else {
+        onOffline();
+      }
+    }, 5000);
+  }
+}
+
+// call an on<Page>Load() function if it exists 
+function executeCallback() {
+  //alert('fired event isPhoneGapReady:'+isPhoneGapReady+" currentUrl:"+currentUrl);
+  if (isPhoneGapReady) {
+    // get the name of the current html page
+    var pages = currentUrl.split("/");
+    var currentPage = pages[pages.length - 1].
+    slice(0, pages[pages.length - 1].indexOf(".html"));
+
+    // capitalize the first letter and execute the function
+    currentPage = currentPage.charAt(0).toUpperCase() + currentPage.slice(1);
+
+    if (typeof window['on' + currentPage + 'Load'] == 'function') {
+      window['on' + currentPage + 'Load']();
     }
-  }, 5000);
-        
+  }
 }
 
 function onOnline() {
@@ -46,6 +84,20 @@ function onOnline() {
 function onOffline() {
   isConnected = false;
   alert("Please connect your " + device.platform + " to the network!");  
+}
+
+function onPause() {
+  isPhoneGapReady = false;
+
+  // clear the Internet check interval
+  window.clearInterval(internetInterval);
+}
+
+function onResume() {
+  // don't run if phonegap is already ready
+  if (isPhoneGapReady == false) {
+    init(currentUrl);
+  }
 }
 
 function deviceDetection() {
@@ -76,6 +128,11 @@ function networkDetection() {
     }
   }
 }
+
+// This gets called by jQuery mobile when the page has loaded
+$(document).bind("pageload", function(event, data) {
+  init(data.url);
+});
 
 // Set an onload handler to call the init function
 window.onload = init;
