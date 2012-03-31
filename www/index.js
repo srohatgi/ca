e378c5e7156028866e688d4d3677b56d33402358
srohatgi <sumeet_rohatgi@hotmail.com>
2012-03-30 17:53:28 -0700

    in the middle

---
+++ www/index.js
@@ -0,0 +1,9 @@
+function onIndexLoad() {
+  //alert('calling http://maps.googleapis.com/maps/api/js?sensor=true&callback=getGeolocation')
+  if (isConnected) {
+    // load folders and activities
+    
+  } else {
+    alert("Must be connected to the Internet");
+  }
+}
