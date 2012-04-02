5dc0d01a03125fed9e32548b7cdf1a9a7e8e568a
srohatgi <sumeet_rohatgi@hotmail.com>
2012-04-01 22:05:31 -0700

    adding native TabBar control

--- www/index.js
+++
@@ -1,9 +0,0 @@
-function onIndexLoad() {
-  //alert('calling http://maps.googleapis.com/maps/api/js?sensor=true&callback=getGeolocation')
-  if (isConnected) {
-    // load folders and activities
-    
-  } else {
-    alert("Must be connected to the Internet");
-  }
-}
