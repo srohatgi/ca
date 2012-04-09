f76bb1d3ff71bd2d27bc5df54b2635df611559ba
srohatgi <sumeet_rohatgi@hotmail.com>
2012-04-09 15:02:15 -0700

    moved all js code in .js file

--- www/screen.js
+++ www/screen.js
@@ -1,4 +1,10 @@
 function onScreenLoad() {
   //alert('inside onScreenLoad()');
-  if ( PUSH_SERVER_IP ) $('#push-ip').val(PUSH_SERVER_IP);
+  if ( PUSH_SERVER_IP ) {
+    $('#push-ip').val(PUSH_SERVER_IP);
+  }
+  $('#push-ip-submit').click(function () {
+    setupPushIP($("#push-ip").val());
+    return true;
+  }); 
 }
